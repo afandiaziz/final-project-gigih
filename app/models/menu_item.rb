e33436079ed13d93545ba80fd4e3224476a096bc
afandiaziz <afandiaziz46@gmail.com>
2022-04-16 17:13:11 +0700

    validation model MenuItem

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,8 @@
 class MenuItem < ApplicationRecord
     has_many :ItemCategories
     validates_associated :ItemCategories
+    
+    validates :name, presence: true, uniqueness: true
+    validates :description, length: { maximum: 150 }
+    validates :price, numericality: true, comparison: { greater_than_or_equal_to: 0.01 }
 end
