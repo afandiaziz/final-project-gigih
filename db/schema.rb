5c33ac382c81a88c20fc1d8d45212a2fb30b5265
afandiaziz <afandiaziz46@gmail.com>
2022-04-16 17:06:51 +0700

    migration add column description to menu_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_16_062228) do
+ActiveRecord::Schema.define(version: 2022_04_16_100444) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_04_16_062228) do
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
 end
