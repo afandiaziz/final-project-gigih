956e6e3e3afefe0bedaced56d968825dcef0b862
afandiaziz <afandiaziz46@gmail.com>
2022-04-16 17:28:29 +0700

    categories add route show by param id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < ApplicationController
     def index
         render json: Category.all, status: :ok
     end
+    def show
+        category = Category.find(params[:id])
+        render json: category, status: :ok
+    end
     def create
         category = Category.create(category_params)
         if category.save
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :categories, only: [:create, :index, :update, :destroy]
+  resources :categories, only: [:create, :index, :update, :destroy, :show]
 end
