c18129be69ab43d3b3771c4a6efacf13872679de
afandiaziz <afandiaziz46@gmail.com>
2022-04-16 22:26:03 +0700

    unit test model category 'update'

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,32 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-    it 'is invalid without a name' do
-        category = Category.new(
-            name: nil,
-        )
-        category.valid?
-        expect(category.errors[:name]).to include("can't be blank")
-    end
-    
-    it 'is valid new category' do
-        category = Category.new(
-            name: 'Makanan'
-        )
-        expect(category).to be_valid
+    describe '-- CREATE category' do
+        it 'is invalid create without a name' do
+            category = Category.create(
+                name: nil,
+            )
+            category.valid?
+            expect(category.errors[:name]).to include("can't be blank")
+        end
+        
+        it "is invalid create with a duplicate name" do
+            category1 = Category.create(
+                name: 'Makanan'
+            )
+            category2 = Category.create(
+                name: 'Makanan'
+            )
+        
+            category2.valid?
+            expect(category2.errors[:name]).to include("has already been taken")
+        end
+        
+        it 'is valid create new category' do
+            category = Category.create(
+                name: 'Makanan'
+            )
+            expect(category).to be_valid
+        end
     end
 
-    # it "is invalid with a duplicate name" do
-    #     foodCategory1 = Category.create(
-    #     name: 'Makanan'
-    #     )
+    describe '-- UPDATE category' do
+        it 'is invalid update without a name' do
+            category = Category.create(
+                name: "Makanan",
+            )
+            category.update(
+                name: nil,
+            )
+            category.valid?
+            expect(category.errors[:name]).to include("can't be blank")
+        end
         
-    #     foodCategory2 = Category.new(
-    #     name: 'Makanan'
-    #     )
-
-    #     foodCategory2.valid?
+        it "is invalid with a duplicate name" do
+            category1 = Category.create(
+                name: 'Makanan'
+            )
+            category2 = Category.create(
+                name: 'Makanan 1'
+            )
+            category2.update(
+                name: 'Makanan'
+            )
         
-    #     expect(foodCategory2.errors[:name]).to include("has already been taken")
-    # end
+            category2.valid?
+            expect(category2.errors[:name]).to include("has already been taken")
+        end
+        
+        it 'is valid new category' do
+            category = Category.create(
+                name: 'Makanan'
+            )
+            category.update(
+                name: 'Makanan 1'
+            )
+            expect(category).to be_valid
+        end
+    end
 end
