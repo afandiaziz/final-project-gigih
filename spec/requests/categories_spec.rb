e50bab2692e636dc023aaffefc092b21adf05d33
afandiaziz <afandiaziz46@gmail.com>
2022-04-17 05:01:16 +0700

    get all categories

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    # pending "add some examples (or delete) #{__FILE__}"
+  # Initialize test data
+  let!(:categories) { create_list(:category, 5) }
+  let(:category_id) { categories.first.id }
+
+  describe 'GET /categories' do
+      before { get '/categories' }
+      it 'return articles' do
+          expect(JSON.parse(response.body)).not_to be_empty
+          expect(JSON.parse(response.body).size).to eq(5)
+      end
+      
+      it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+      end
   end
 end
