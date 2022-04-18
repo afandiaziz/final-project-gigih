8351584d6e08b14e437694553b62c526420d16ba
afandiaziz <afandiaziz46@gmail.com>
2022-04-18 20:04:48 +0700

    Unit Testing requests item categories

--- spec/factories/item_categories.rb
+++ spec/factories/item_categories.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
-  factory :item_category do
-    item_id { Faker::Number.between(from: 1, to: 20) }
-    category_id { Faker::Number.between(from: 1, to: 20) }
-  end
-end
+    factory :item_category do
+        category_id { Faker::Number.between(from: 1, to: 5) }
+        item_id { Faker::Number.between(from: 1, to: 2) }
+        # category_id { 1 }
+        # item_id { 1 }
+        # item_id { 
+        #     create(:menu_item)
+        #     MenuItem.first.id 
+        # }
+        # category_id { create(:item_category) }
+    end
+end 
--- spec/requests/item_categories_spec.rb
+++ spec/requests/item_categories_spec.rb
@@ -1,7 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe "ItemCategories", type: :request do
-  describe "GET /index" do
-    # pending "add some examples (or delete) #{__FILE__}"
-  end
+    let!(:menuItems) { create_list(:menu_item, 2) }
+    let!(:categories) { create_list(:category, 5) }
+    let!(:item_categories) {
+        ItemCategory.insert_all([
+            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
+            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
+            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
+            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
+            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
+        ])
+    }
+    let(:itemCategory_id) { ItemCategory.first.id }
+    # let!(:item_categories) { create_list(:item_category, 2) }
+    # let!(:item_categories) { create(:item_category) }
+    describe "GET /index" do
+        before { get '/item_categories' }
+        it 'return item categories' do
+            expect(JSON.parse(response.body)).not_to be_empty
+            expect(JSON.parse(response.body).size).to eq(5)
+        end
+        
+        it 'return status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+    describe 'GET /item_categories/:id' do
+        before { get "/item_categories/#{itemCategory_id}" }
+    
+        context 'when the record exists' do
+            it 'return the item category' do
+                expect(JSON.parse(response.body)).not_to be_empty
+                expect(JSON.parse(response.body)['id']).to eq(itemCategory_id)
+            end
+        
+            it 'return status code 200' do
+                expect(response).to have_http_status(200)
+            end
+        end
+    
+        context 'when the record does not exist' do
+            let(:itemCategory_id) { 100 }
+        
+            it 'return status code 404' do
+                expect(response).to have_http_status(404)
+                expect(JSON.parse(response.body)['message']).to match("Couldn't find ItemCategory with 'id'=100")
+            end
+        end
+    end
+    
+    # describe 'POST /item_categories' do
+    #     context 'the request is invalid' do
+    #         it "Category id can't be blank" do
+    #             post '/item_categories', params: { 
+    #                 category_id: nil,
+    #                 item_id: Faker::Number.between(from: 1, to: 2)
+    #             }
+    #             expect(response).to have_http_status(422)
+    #             expect(JSON.parse(response.body)['message']).to match("Validation failed: Name can't be blank")
+    #         end
+        
+    #     end
+
+    #     context 'the request is valid' do
+    #         it 'created a menu item' do
+    #             post '/item_categories', params: {  
+    #                 category_id: Faker::Number.between(from: 1, to: 5),
+    #                 item_id: Faker::Number.between(from: 1, to: 2)
+    #             } 
+    #             expect(JSON.parse(response.body)['name']).to eq('Nasi Kuning')
+    #             expect(response).to have_http_status(201)
+    #         end
+    #     end
+    # end
+
+    # describe 'PUT /item_categories/:id' do
+    #     context 'when the record exists' do
+    #         before { put "/item_categories/#{itemCategory_id}", params: { category_id: Faker::Number.between(from: 1, to: 5) } }
+        
+    #         it 'updated the record' do
+    #             expect(response.body).to be_empty
+    #         end
+        
+    #         it 'return status code 204' do
+    #             expect(response).to have_http_status(204)
+    #         end
+    #     end
+    # end
+
+    describe 'DELETE /item_categories/:id' do
+        before { delete "/item_categories/#{itemCategory_id}" }
+    
+        it 'return status code 204' do
+            expect(response).to have_http_status(204)
+        end
+    end
 end
