06857946302b2e47283c2dcb46cfd80e43860599
afandiaziz <afandiaziz46@gmail.com>
2022-04-20 22:40:49 +0700

    change init data unit testing requests item categories

--- spec/requests/item_categories_spec.rb
+++ spec/requests/item_categories_spec.rb
@@ -3,17 +3,8 @@ require 'rails_helper'
 RSpec.describe "ItemCategories", type: :request do
     let!(:menuItems) { create_list(:menu_item, 2) }
     let!(:categories) { create_list(:category, 5) }
-    let!(:item_categories) {
-        ItemCategory.insert_all([
-            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
-            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
-            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
-            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
-            { category_id: Faker::Number.between(from: 1, to: 5), item_id: Faker::Number.between(from: 1, to: 2) },
-        ])
-    }
+    let!(:item_categories) { create_list(:item_category, 5) }
     let(:itemCategory_id) { ItemCategory.first.id }
-    # let!(:item_categories) { create_list(:item_category, 2) }
     # let!(:item_categories) { create(:item_category) }
     describe "GET /index" do
         before { get '/item_categories' }
